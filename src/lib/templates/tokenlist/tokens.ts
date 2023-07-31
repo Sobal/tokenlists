6e417893e38ded2f75dad4c6a19eb5d5c63d342e
Joeh <joeh@alchemist.wtf>
2023-07-31 22:21:05 +0100

    update networks

--- src/lib/templates/tokenlist/tokens.ts
+++ src/lib/templates/tokenlist/tokens.ts
@@ -1,11 +1,6 @@
 import { Network, TokensForList } from '../../types'
 
 export const tokens: TokensForList = {
-  [Network.Ethereum]: [],
-  [Network.Goerli]: [],
-  [Network.Polygon]: [],
-  [Network.Arbitrum]: [],
-  [Network.Optimism]: [],
-  [Network.Gnosis]: [],
-  [Network.Zkevm]: [],
+  [Network.Base]: [],
+  [Network.BaseGoerli]: [],
 }
